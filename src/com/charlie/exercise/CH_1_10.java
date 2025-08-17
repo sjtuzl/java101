8f7762ffe1bb0169eec3b7c0ae054e09a5f5e439
daftendirekt-efz <charliezhangcn@outlook.com>
2025-08-17 20:44:23 +0800

    Delete src/com/charlie/exercise directory

--- src/com/charlie/exercise/CH_1_10.java
+++
@@ -1,9 +0,0 @@
-package com.charlie.exercise;
-
-public class CH_1_10 {
-    public static void main(String[] args) {
-        double time = 45.500 / 60.000;
-        double distance = 14.000 / 1.6;
-        System.out.println(distance / time);
-    }
-}
--- src/com/charlie/exercise/CH_1_9.java
+++
@@ -1,8 +0,0 @@
-package com.charlie.exercise;
-
-public class CH_1_9 {
-    public static void main(String[] args) {
-        System.out.println(4.5 * 7.9);
-        System.out.println(2 * 4.5 + 2 * 7.9);
-    }
-}
